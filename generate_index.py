7c9604e190a2a1daa505a368ccf079678574ce5f
grego <gregoriofaerman@gmail.com>
2023-10-07 21:16:31 -0300

    actualizaciones en generate_index

--- generate_index.py
+++ generate_index.py
@@ -1,5 +1,7 @@
 import os
 from bs4 import BeautifulSoup
+import markdown
+import html
 
 # Obtener la lista de archivos de la carpeta 'pages'
 files = os.listdir("pages")
@@ -14,29 +16,34 @@ articles_html = ""
 for file_name in files:
     if file_name.endswith(".md"):
         with open(os.path.join("pages", file_name), "r", encoding="utf-8") as file:
-            # Analizar el contenido Markdown
+            # Leer el contenido Markdown
             content = file.read()
-            # Puedes utilizar una biblioteca Markdown para convertir el contenido Markdown a HTML si es necesario
+
+            # Convertir Markdown a HTML
+            content_html = markdown.markdown(content)
 
             # Extraer la fecha y el título del nombre del archivo
             _, date, title = file_name.split("-", 2)
             date = date.replace("_", "-")
             title = title.replace(".md", "").replace("_", " ")
 
+            # HTML-escapar el título
+            title = html.escape(title)
+
             # Generar el enlace al artículo en el índice
-            index_html += f'<li><a href="{file_name}">{date} - {title}</a></li>'
+            index_html += f'<li><a href="pages/{file_name}">{date} - {title}</a></li>'
 
             # Generar el contenido del artículo
-            articles_html += f'<article id="{file_name}">{content}</article>'
+            articles_html += f'<article id="{file_name}">{content_html}</article>'
 
 # Insertar el índice y el contenido en index.html
 with open("index.html", "r+", encoding="utf-8") as index_file:
     soup = BeautifulSoup(index_file, "html.parser")
     index_list = soup.find("ul")
-    index_list.extend(BeautifulSoup(index_html, "html.parser"))
+    index_list.append(BeautifulSoup(index_html, "html.parser"))
 
     main_content = soup.find("main")
-    main_content.extend(BeautifulSoup(articles_html, "html.parser"))
+    main_content.append(BeautifulSoup(articles_html, "html.parser"))
 
     # Guardar los cambios en index.html
     index_file.seek(0)
